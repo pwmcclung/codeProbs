9d1bf8005520f600accdca55303a3ca9a253e71e
pwmcclung <pwmcclung@aol.com>
2023-08-10 08:24:36 -0600

    added triangle to git

---
+++ triangle.py
@@ -0,0 +1,11 @@
+def triangle_type(a, b, c):
+    biggest = max(a,b,c)
+    
+    if a+b+c <= 2 * biggest:
+        return 0
+    if 2 * biggest * biggest  == a * a + b * b + c * c:
+        return 2
+    if 2 * biggest * biggest  > a * a + b * b + c * c:
+        return 3
+    else:
+        return 1
