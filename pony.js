0986813cb0d58499d5fa738c3c9cbb26c4be723a
pwmcclung <pwmcclung@aol.com>
2023-09-21 06:03:15 -0600

    added pony to git

---
+++ pony.js
@@ -0,0 +1,14 @@
+function riders(stations) {
+    let riders = 0;
+    let stationCount = 0;
+    let milesSum = 0;
+    for (let i = 0; i <  stations.length; i++){
+      milesSum += stations[i];
+      if (milesSum > 100){
+        riders++;
+        milesSum = 0;
+        i--;
+      }
+    }
+    return riders + 1;
+  }
