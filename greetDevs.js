34ab2caae680635867017d9cda69d657d2bf3857
pwmcclung <pwmcclung@aol.com>
2023-12-15 07:56:26 -0700

    added greet devs to git

---
+++ greetDevs.js
@@ -0,0 +1,7 @@
+function greetDevelopers(list) {
+    list.forEach(x => {
+      x.greeting = `Hi ${x.firstName}, what do you like the most about ${x.language}?`;
+    });
+    
+    return list;
+  }
