780cd72a9aa9ed0dc1339b43b9ab59c0ac48939f
pwmcclung <pwmcclung@aol.com>
2023-12-15 06:38:24 -0700

    added coding meet 1 to git

---
+++ codingMeet1.js
@@ -0,0 +1,10 @@
+function countDevelopers(list) {
+    let count = 0;
+    for (let i = 0; i < list.length; i++){
+      if (list[i].continent === 'Europe' && list[i].language === 'JavaScript'){
+        count++;
+      }
+      
+    }
+    return count;
+  }
