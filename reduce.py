56991cc9a3f0ed4b12e6c44200d6c3164c9b0275
pwmcclung <pwmcclung@aol.com>
2024-01-10 05:59:31 -0700

    added reduce to git

---
+++ reduce.py
@@ -0,0 +1,14 @@
+function reduce(fraction) {
+  let min = Math.min(...fraction)
+  let arr = [];
+  let start = 1;
+  for (let i = 1; i <= min; i++){
+    if (fraction[0] % i == 0 && fraction[1] % i == 0){
+      arr.push(i);
+    }
+  }
+  let biggest = arr[arr.length-1];
+  let times1 = fraction[0] / biggest;
+  let times2 = fraction[1] / biggest;
+  return [times1,times2];
+}
