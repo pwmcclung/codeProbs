af40e89ee601f91bb3ca4699cb2319cf3a9d46e4
pwmcclung <pwmcclung@aol.com>
2024-01-29 06:20:10 -0700

    added running sum to git

---
+++ runningSum.js
@@ -0,0 +1,9 @@
+var runningSum = function(nums) {
+    let sum = 0;
+    let arr = [];
+    for (let i = 0; i < nums.length; i++){
+        sum+=nums[i];
+        arr.push(sum);
+    }
+    return arr;
+};
