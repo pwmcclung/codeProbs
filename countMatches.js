e8a760e644a95d2a6627f29ff06b6448e5bafdb3
pwmcclung <pwmcclung@aol.com>
2024-01-31 06:48:43 -0700

    added count matches to git

---
+++ countMatches.js
@@ -0,0 +1,13 @@
+var countMatches = function(items, ruleKey, ruleValue) {
+    let count = 0;
+    for (let i = 0; i < items.length; i++){
+        if (ruleKey == 'type' && ruleValue == items[i][0]){
+            count++;
+        }else if (ruleKey == 'color' && ruleValue == items[i][1]){
+            count++;
+        }else if (ruleKey == 'name' && ruleValue == items[i][2]){
+            count++;
+        }
+    }
+    return count;
+};
