1d64c737f94c46d498c1ee7c8b65385969fed34c
pwmcclung <pwmcclung@aol.com>
2024-03-10 10:48:50 -0600

    added good pairs to git

---
+++ good_pairs.py
@@ -0,0 +1,7 @@
+def numIdenticalPairs(self, nums: List[int]) -> int:
+    count_of_good_pairs = 0
+    times = Counter()
+    for number in nums:
+        count_of_good_pairs  += times[number]
+        times[number] += 1
+    return count_of_good_pairs
