ecdffb228b963982e07e55725b706f35cd4f42c5
pwmcclung <pwmcclung@aol.com>
2024-06-26 08:14:41 -0600

    added collatz to git

---
+++ collatzL.js
@@ -0,0 +1,14 @@
+function collatz(n) {
+    let arr = [n,];
+    let count = 0;
+    while (n != 1){
+      if (n % 2 == 0){
+        n = n / 2;
+        arr.push(n);
+      }else if (n % 2 != 0){
+        n = n * 3 + 1;
+        arr.push(n)
+      } 
+      }
+    return arr.length;
+  }
