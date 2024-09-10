d5b90634aadaa9518bb51530ced2c4b3fe9cd0bb
pwmcclung <pwmcclung@aol.com>
2024-09-10 08:16:08 -0600

    added mean vs median ot git

--- meanVsMedian.js
+++ meanVsMedian.js
@@ -2,8 +2,8 @@ function meanVsMedian(numbers) {
     let newNums = numbers.sort(function(a,b){return a-b});
     let len = numbers.length;
     let mean = (numbers.reduce((a,b) => a + b,0) / len);
-    let midPoint = Math.floor(len / 2)
-    let median = newNums[midPoint]
+    let midPoint = Math.floor(len / 2);
+    let median = newNums[midPoint];
     if (median == mean){
       return 'same';
     }else if (median > mean){
